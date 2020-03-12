b0be666d8b3af01d32604bc03ea82a65dfd5be40
mckeekaylie <mckeekaylie@gmail.com>
2020-03-12 10:35:21 -0400

    pushing server

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.PASS, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
